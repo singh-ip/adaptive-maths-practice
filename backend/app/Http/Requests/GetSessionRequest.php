d743ced583b60f2c63db1a68f1be03739cb84d07
Inderpreet Singh <inderpreetnarula@yahoo.com>
2026-02-25 16:01:53 +0530

    GetSessionRequest

---
+++ backend/app/Http/Requests/GetSessionRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class GetSessionRequest extends FormRequest
+{
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'session_id' => $this->route('sessionId'),
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'session_id' => 'required|integer|exists:sessions,id',
+        ];
+    }
+}
